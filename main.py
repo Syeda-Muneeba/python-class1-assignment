da0eeb1360c5f25523ab4fc17c39a5d8aa27d172
Syed zadi <160313963+Syeda-Muneeba@users.noreply.github.com>
2025-03-02 22:49:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# First Class Hello World
+# print ("Hello World from Muneeba!")
+
+# def giaic():
+#     print ("Hello World from Python!")
+
+
+# Pycache Example
+def greet(name):
+    return "Hello, {name}!"
+
+print(greet("Muneeba"))
+
+
+
+# data types
+
+# string
+name = "Muneeba"
+
+# integer
+age = 20
+
+# float
+height = 5.8
+
+# boolean
+giaic_student = True
+
+# list
+grades = [15, 23, 40, 57, 54]
+
+# tuple
+coordinates = (5, 7)
+
+# dictionary
+person = {"name": "Muneeba", "age": 20, "height": 5.8}
+
+# set
+unique_numbers = {2, 1, 5, 4, 7}
+
+print(name, age, height, giaic_student, grades, coordinates, person, unique_numbers)
